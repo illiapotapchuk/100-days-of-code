713bd02fa35152325314f81e197d9f1304f8e465
unknown <potapchuk.illia@gmail.com>
2017-10-08 17:11:28 -0500

    Added a chocolate fiest solution

---
+++ chocolateFeast.js
@@ -0,0 +1,43 @@
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function main() {
+    var t = parseInt(readLine());
+    for(var a0 = 0; a0 < t; a0++){
+        var n_temp = readLine().split(' ');
+        var n = parseInt(n_temp[0]);
+        var c = parseInt(n_temp[1]);
+        var m = parseInt(n_temp[2]);
+
+        var numberOfWraps = parseInt(n/c);
+        var eatenChocolates = numberOfWraps;
+        var boughtWithWraps;
+        while(numberOfWraps >= m) {
+            boughtWithWraps = parseInt(numberOfWraps/m);
+            eatenChocolates += boughtWithWraps;
+            numberOfWraps = numberOfWraps%m + boughtWithWraps;
+        }
+
+        console.log(eatenChocolates);
+    }
+
+}
